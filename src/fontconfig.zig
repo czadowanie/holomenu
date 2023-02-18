baba858800b7b92d45a736dbac209128a8464d0f
czadowanie <nikolakisala@gmail.com>
2023-02-18 20:25:51 +0100

    cleanup fontconfig integration

---
+++ src/fontconfig.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+
+const c = @cImport({
+    @cInclude("fontconfig/fontconfig.h");
+});
+
+pub const FontConfig = struct {
+    filepath: [:0]u8,
+    size: i32,
+    allocator: std.mem.Allocator,
+
+    /// cleanup with `deinit`
+    pub fn parse_and_resolve(allocator: std.mem.Allocator, fc_pattern: [*c]const u8) !@This() {
+        const conf = c.FcInitLoadConfigAndFonts().?;
+
+        const pattern = c.FcNameParse(fc_pattern).?;
+        defer c.FcPatternDestroy(pattern);
+
+        if (c.FcConfigSubstitute(conf, pattern, c.FcMatchPattern) != c.FcTrue) {
+            std.log.err("failed to config substitute", .{});
+        }
+        c.FcDefaultSubstitute(pattern);
+
+        const font_set = c.FcFontSetCreate().?;
+        defer c.FcFontSetDestroy(font_set);
+
+        const object_set = c.FcObjectSetBuild(
+            c.FC_FAMILY,
+            c.FC_STYLE,
+            c.FC_FILE,
+            c.FC_SIZE,
+            @intToPtr([*c]const u8, 0),
+        ).?;
+        defer c.FcObjectSetDestroy(object_set);
+
+        var result: c.FcResult = undefined;
+        const font_patterns = c.FcFontSort(conf, pattern, c.FcTrue, null, &result).?;
+        if (@ptrCast(*const c.FcFontSet, font_patterns).nfont == 0) {
+            std.log.err("could not find any fonts", .{});
+        }
+        defer c.FcFontSetSortDestroy(font_patterns);
+
+        const font_pattern = c.FcFontRenderPrepare(
+            conf,
+            pattern,
+            @ptrCast(*const c.FcFontSet, font_patterns).fonts[0],
+        ).?;
+        _ = c.FcFontSetAdd(font_set, font_pattern);
+
+        const font = c.FcPatternFilter(@ptrCast(*const c.FcFontSet, font_set).fonts[0], object_set);
+        defer c.FcPatternDestroy(font);
+
+        var value: c.FcValue = undefined;
+
+        _ = c.FcPatternGet(font, c.FC_FILE, 0, &value);
+        const value_filepath = std.mem.sliceTo(@ptrCast([*c]const u8, value.u.f), 0);
+        var filepath = try allocator.allocSentinel(u8, value_filepath.len, 0);
+        std.mem.copy(u8, filepath, value_filepath);
+
+        _ = c.FcPatternGet(font, c.FC_SIZE, 0, &value);
+
+        return .{
+            .filepath = filepath,
+            .size = @floatToInt(i32, value.u.d),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: @This()) void {
+        self.allocator.free(self.filepath);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 
+const FontConfig = @import("fontconfig.zig").FontConfig;
+
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
     @cInclude("SDL2/SDL_ttf.h");
-    @cInclude("fontconfig/fontconfig.h");
 });
 
 pub fn Vec2(comptime T: type) type {
@@ -30,56 +31,10 @@ pub const HoloMenuConfig = struct {
     height: u32 = 12,
 };
 
-pub const FontConifg = struct {
-    filepath: [256]u8,
-    size: i32,
-
-    fn parse_and_resolve(fc_pattern: [*c]const u8) @This() {
-        // TODO: cleanup
-
-        const conf = c.FcInitLoadConfigAndFonts().?;
-        const pattern = c.FcNameParse(fc_pattern).?;
-        if (c.FcConfigSubstitute(conf, pattern, c.FcMatchPattern) != c.FcTrue) {
-            std.log.err("failed to config substitute", .{});
-        }
-        c.FcDefaultSubstitute(pattern);
-
-        const font_set = c.FcFontSetCreate().?;
-        const object_set = c.FcObjectSetBuild(c.FC_FAMILY, c.FC_STYLE, c.FC_FILE, c.FC_SIZE, @intToPtr([*c]const u8, 0)).?;
-
-        var result: c.FcResult = undefined;
-        const font_patterns = c.FcFontSort(conf, pattern, c.FcTrue, null, &result).?;
-        if (@ptrCast(*const c.FcFontSet, font_patterns).nfont == 0) {
-            std.log.err("could not find any fonts", .{});
-        }
-
-        const font_pattern = c.FcFontRenderPrepare(conf, pattern, @ptrCast(*const c.FcFontSet, font_patterns).fonts[0]).?;
-        _ = c.FcFontSetAdd(font_set, font_pattern);
-
-        const font = c.FcPatternFilter(@ptrCast(*const c.FcFontSet, font_set).fonts[0], object_set);
-
-        var value: c.FcValue = undefined;
-
-        _ = c.FcPatternGet(font, c.FC_FILE, 0, &value);
-        var filepath = std.mem.zeroes([256]u8);
-        var i: usize = 0;
-        while ((@ptrCast([*c]const u8, value.u.f)[i] != 0) and (i < 255)) : (i += 1) {
-            filepath[i] = @ptrCast([*c]const u8, value.u.f)[i];
-        }
-
-        _ = c.FcPatternGet(font, c.FC_SIZE, 0, &value);
-
-        std.log.info("{s}", .{filepath});
-        std.log.info("{}", .{value.type});
-
-        return .{
-            .filepath = filepath,
-            .size = @floatToInt(i32, value.u.d),
-        };
-    }
-};
-
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         std.log.err("failed to init SDL: {s}", .{c.SDL_GetError()});
         return;
@@ -119,9 +74,12 @@ pub fn main() !void {
     }
     var input = try stdin.readAllAlloc(alloc, 1024 * 1024);
 
-    const fc = FontConifg.parse_and_resolve("Coming Soon:size=14");
-    std.log.info("{}", .{fc});
-    const font = c.TTF_OpenFont(&fc.filepath, fc.size).?;
+    const fc = try FontConfig.parse_and_resolve(allocator, "Iosevka:bold:italic:size=14");
+    defer fc.deinit();
+
+    std.log.info("font path: \"{s}\", font size: {}", .{ fc.filepath, fc.size });
+
+    const font = c.TTF_OpenFont(fc.filepath.ptr, fc.size).?;
     defer c.TTF_CloseFont(font);
 
     var running = true;
